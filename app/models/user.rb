698e0b2c8a8610dfd697e34de8a762f430f88bd9
Kauan Carvalho <kauan_calasans@hotmail.com>
2019-12-04 13:43:47 -0300

    Primeiro commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+         def password_complexity
+          return if password.blank? || password =~ /(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-])/
+      
+          errors.add :password, 'É necessário que sua senha possua: Letra maiuscula, letra minuscul, um numero e um caracatere especial'
+        end
 end
---
+++ db/migrate/20191204122231_add_full_name_to_users.rb
@@ -0,0 +1,14 @@
+class AddFullNameToUsers < ActiveRecord::Migration[6.0]
+  def change
+    add_column :users, :fullname, :string
+    add_column :users, :razaosocial, :string
+    add_column :users, :pf_pj, :string
+    add_column :users, :cpf, :string
+    add_column :users, :cnpj, :string
+    add_column :users, :rua, :string
+    add_column :users, :cep, :string
+    add_column :users, :bairro, :string
+    add_column :users, :cidade, :string
+    add_column :users, :estado, :string
+  end
+end
