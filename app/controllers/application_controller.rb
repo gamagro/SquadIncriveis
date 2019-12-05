90a559b54df74b68a165c2561ca3c7f1add6f78a
gamagro <gabriel.carvalho@youse.com.br>
2019-12-05 11:09:42 -0300

    ultima trad

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,18 +3,15 @@ class ApplicationController < ActionController::Base
 
     before_action :authenticate_user!
     before_action :configure_permitted_parameters, if: :devise_controller?
-
 #
 #          validates_presence_of :name, message: 'não pode ser deixado em branco'
 #          validates_presence_of :email, message: 'não pode ser deixado em branco'
 #          validates_presence_of :password, message: 'não pode ser deixado em branco'
 #    
-
      protected
 
           def configure_permitted_parameters
                devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password, :fullname, :razaosocial, :pf_pj, :cpf, :cnpj, :rua, :cep, :bairro, :cidade, :estado)}
-
                devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :email, :password, :current_password, :fullname, :razaosocial, :pf_pj, :cpf, :cnpj, :rua, :cep, :bairro, :cidade, :estado)}
           end
 
