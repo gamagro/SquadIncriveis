70b713ed20a38fed56e28223f750c2fcd8dccd2b
Gabi <gabriela.prado@youse.com.br>
2019-12-11 15:48:24 -0300

    alteracoes do design cadastro e login

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,6 +1,6 @@
 module UsersHelper
     def pfpj
-        [["Selecione",""], ["Pessoa Física", "PF"], ["Pessoa jurídica", "PJ"]] 
+        [["Selecione",""], ["Pessoa Física", "PF"], ["Pessoa Jurídica", "PJ"]] 
     end
 
     def states
