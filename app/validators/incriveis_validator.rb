6322695e302ff5d6503f0fb6f0890a369dd8e581
gamagro <gabriel.carvalho@youse.com.br>
2019-12-17 15:11:44 -0300

    error message ok

--- app/validators/incriveis_validator.rb
+++ app/validators/incriveis_validator.rb
@@ -2,37 +2,37 @@ class IncriveisValidator < ActiveModel::Validator
     def validate(record)
         if record.pf_pj == "PF"
             if !BRDocuments::CPF.valid?(record.cpf)
-                record.errors[:cpf] << "CPF Inválido"
+                record.errors[:cpf] << "inválido."
             end
             if record.fullname == ""
-                record.errors[:fullname] << "O campo 'Nome completo' não pode ser nulo."
+                record.errors[:fullname] << "não pode ser nulo."
             end
             if record.fullname.length > 50
-                record.errors[:fullname] << "O campo 'Nome completo' deve possuir menos que 51 caracteres."
+                record.errors[:fullname] << "deve possuir menos que 51 caracteres."
             end
             if !record.fullname.match(/\A[[:alpha:][:blank:]]+\z/)
-                record.errors[:fullname] << "O campo 'Nome completo' deve conter apenas letras e espaços."
+                record.errors[:fullname] << "deve conter apenas letras e espaços."
             end
             record.cnpj = nil
             record.razaosocial = nil
         elsif record.pf_pj == "PJ"
             if !BRDocuments::CNPJ.valid?(record.cnpj)
-                record.errors[:cnpj] << "CNPJ Inválido"
+                record.errors[:cnpj] << "inválido."
             end
             if record.razaosocial == ""
-                record.errors[:razaosocial] << "O campo 'Razão social' não pode ser nulo."
+                record.errors[:razaosocial] << "não pode ser nulo."
             end
             if record.cep == ""
-                record.errors[:cep] << "O campo 'CEP' não pode ser nulo."
+                record.errors[:cep] << "não pode ser nulo."
             end 
             record.cpf = nil
             record.fullname = nil
         end
         if !record.password.match(/(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*\W+).*$/)
-            record.errors[:password] << "A senha de ve conter um caractere especial, uma letra maiúscula, uma letra minúscula e um número."
+            record.errors[:password] << "deve conter um caractere especial, uma letra maiúscula, uma letra minúscula e um número."
         end
         if !record.email.match(/\A[^@\s]+@youse.com.br/);
-            record.errors[:email] << "O seu email deve ser no formato 'fulano@youse.com.br'."
+            record.errors[:email] << "deve ser no formato 'fulano@youse.com.br'."
         end
         if record.pf_pj == ""
             record.errors[:pf_pj] << "Selecione o tipo de pessoa"
