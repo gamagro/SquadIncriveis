0f79c4d5866946effe256ebfa7fe6b64f808af6c
Kauan Carvalho <kauan_calasans@hotmail.com>
2020-01-16 19:00:48 -0300

    upgrade

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   skip_before_action :authenticate_user!, only: [:emailcheck, :cpfcheck, :cnpjcheck]
+  skip_before_action :verify_authenticity_token
 
   def index
     @users = User.all
